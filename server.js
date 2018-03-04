d468fe04538b821517086e4b2484a201b499e430
Brea Torres <abreaw328@gmail.com>
2018-03-04 16:11:32 -0500

    mongodb setup models and connection

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const path = require("path");
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const routes = require("./routes");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Add routes, both API and view
+// app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -14,6 +26,17 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/phoenixdb",
+  {
+    useMongoClient: true
+  }
+);
+
+// Start the API / express server
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
