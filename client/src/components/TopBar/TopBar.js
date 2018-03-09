b026b58cdfc6deb54d393bb4e16b1d9700ce5727
Miriam Weirich <weirich.mir@gmail.com>
2018-03-09 15:52:29 -0500

    adding button and box

--- client/src/components/TopBar/TopBar.js
+++ client/src/components/TopBar/TopBar.js
@@ -3,18 +3,20 @@ import 'bulma/css/bulma.css';
 import Section from 'react-bulma-components/lib/components/section';
 import Container from 'react-bulma-components/lib/components/container';
 import Heading from 'react-bulma-components/lib/components/heading';
+import Button from 'react-bulma-components/lib/components/button';
+import Box from 'react-bulma-components/lib/components/box';
 
 const TopBar = props =>
 
 <div>
     <Section>
-        <Container>
-            <p className="bd-notification is-success">
-                <Heading size={5} renderAs="p">
-                This is where stuff will live
-                </Heading>
-            </p>
-        </Container>
+        <Box>
+            <Container>
+                <Button>
+                    New Activity
+                </Button>
+            </Container>
+        </Box>
     </Section>
 </div>
 
