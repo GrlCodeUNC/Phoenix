db9610a32114defc1e42284390bc51f2856a336d
Anisha Patel <patella08@yahoo.com>
2018-03-15 20:21:34 -0400

    profile pic edit

--- client/src/components/UserPhoto/UserPhoto.js
+++ client/src/components/UserPhoto/UserPhoto.js
@@ -2,27 +2,18 @@ import React, { Component } from "react";
 import 'bulma/css/bulma.css';
 import Image from 'react-bulma-components/lib/components/image';
 
-
-const imageStyle = {
-    borderRadius: "50%"
-}
-
-const UserPhoto = props => {
-// Look into the Image component
-return(<Image style={{borderRadius: "50%"}} src={localStorage.getItem("picture")} size={96}/>) 
-
-// const myImage = new Image(80);
-// myImage.src = "picture";
-// document.body.appendChild(myImage);
-
-// const image = $("<img>");
-//     image.addClass("circle").attr("width", "80").attr("src", "picture");
-//     $("#is-one-quarter").append(image);
-
-}
-
-
-
-git 
+let UserPhoto = function statelessFunctionComponentClass(props) {
+    let source = './images/' + props.source;
+  
+    let style = {
+      width: '80px',
+      margin: '10px 5px 0px 5px',
+      borderRadius: "50%"
+    };
+  
+    return (
+      <img src={localStorage.getItem("picture")} style={style} />
+    );
+  };
 
 export default UserPhoto;
