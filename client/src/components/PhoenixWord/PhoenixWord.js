0f9f429d3bb456533991dd9b838a7a96edd29a44
Miriam Weirich <weirich.mir@gmail.com>
2018-03-17 13:34:14 -0400

    phoenix word logo

---
+++ client/src/components/PhoenixWord/PhoenixWord.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "bulma/css/bulma.css";
+import word from "../../PhoenixWordLogo-02.png";
+import "./PhoenixWord.css";
+
+
+class PhoenixWord extends Component {
+
+    render() {
+        let cssClass = '';
+        if (this.props.location === '') {
+            cssClass = 'Word-Dashboard'
+        } else if (this.props.location === 'newActivity') {
+            cssClass = 'Word-NewActivity'
+        }
+          else if (this.props.location === 'description') {
+            cssClass = 'Word-Description'
+        }
+
+        return (
+               <img src={word} className={cssClass} alt="word" style={{position:"fixed", opacity: ".3", bottom:"0px"}}/> 
+        )
+    }
+}
+
+export default PhoenixWord;
+
+
